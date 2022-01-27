d6b88a02abc6c4107d3d9c65f78454393ca3c6b9
Atharv Shingewar <shingewaratharv@gmail.com>
2022-01-27 18:21:54 +0530

    added pin change

--- rasa/actions/query.py
+++ rasa/actions/query.py
@@ -21,3 +21,21 @@ def get_balance(email):
     if len(myresult):
         return myresult[0][0]
     return "Cannot access that data right now!!"
+
+
+def change_pin(email,pin):
+    mydb = connect()
+    mycursor = mydb.cursor()
+    query = "update accounts_card set pin = {} where acc_no_id = (select acc_no from accounts_account where customer_id_id =(select customer_id from accounts_customer where email = '{}'));".format(str(pin),email)
+    mycursor.execute(query)
+    mydb.commit()
+    row_affected = mycursor.rowcount
+    mydb.close()
+    if row_affected:
+        return True
+    return False
+    
+
+# print(change_pin("shingewaratharv@gmail.com",3466))
+
+
